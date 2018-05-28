ed7e52b9bf17477c6458866c3ec565842c388e4f
dingdongxue <644821023@qq.com>
2018-05-28 18:18:30 +0800

    ’修改'

--- src/components/common/timeLine/timeLine.js
+++
@@ -1,9 +0,0 @@
-// $(".timeLine_main .year .list").each(function (e, target) {
-//   var $target=  $(target),
-//     $ul = $target.find("ul");
-//   $target.height($ul.outerHeight()), $ul.css("position", "absolute");
-// });
-// $(".timeLine_main .year>h2>a").click(function(e){
-//   e.preventDefault();
-//   $(this).parents(".year").toggleClass("close");
-// });
--- src/components/home.vue
+++ src/components/home.vue
@@ -49,7 +49,7 @@ export default {
       message: {},
       currentDate: new Date(),
       area: ['350px', '350px'],
-      title: '测试',
+      title: '360全景隆重上线',
       dataList: [
         {title: '2014年', children: [{date: '3月5日', intro: '360全景隆重上线', more: '全新360全景模块上线，3D看房、3D看车、3D看实景，一网打尽'}, {date: '2月26日', intro: '微婚庆行业应用上线', more: '提供更加友好的套餐价格'}]},
         {title: '2015年', children: [{date: '1月', intro: '上线', more: '一网打尽'}, {date: '2月', intro: '微婚庆行上线', more: '提供更加友餐价格'}]}
